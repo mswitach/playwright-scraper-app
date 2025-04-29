87e8819d50bf6182c4a64f1185afcf47658410a4
Marcelo Switach <marceloswitach@Marcelos-MacBook-Air.local>
2025-04-29 18:28:19 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/effective-tests-chart.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
